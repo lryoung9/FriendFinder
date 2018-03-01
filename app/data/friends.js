f60bdc7817779de87ab09f656a9690d3eddff0e0
Lydia Young <lydiaryoung@gmail.com>
2018-02-28 21:29:01 -0700

    added friends array with beginning seeds

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+// Seed friend list
+var friends = [
+	{
+		routeName: "jddorian",
+		name: "J.D. Dorian",
+		image: "https://en.wikipedia.org/wiki/J.D._(Scrubs)#/media/File:Jd_season9.jpg",
+		scores: ["5", "1", "1", "1", "5", "1", "1", "1", "5", "5"]
+	},
+	{
+		routeName: "dwightschrute",
+		name: "Dwight Schrute",
+		image: "https://en.wikipedia.org/wiki/Dwight_Schrute#/media/File:Dwight_Schrute.jpg",
+		scores: ["1", "5", "5", "1", "1", "5", "5", "5", "1", "1"]
+	},
+	{
+		routeName: "johnbender",
+		name: "John Bender",
+		image: "http://www.imdb.com/title/tt0088847/mediaviewer/rm956498944?ft0=name&fv0=nm0000555&ft1=image_type&fv1=still_frame",
+		scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+	}
+]
