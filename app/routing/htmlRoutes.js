33b97b83c835e35a89d84d1d64f4c572769038c5
Lydia Young <lydiaryoung@gmail.com>
2018-02-28 22:59:19 -0700

    html routes complete

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+// DEPENDENCIES
+var path = require("path");
+
+// Routes
+module.exports = function(app) {
+	app.get('/', function (req, res) {
+		res.sendFile(path.join(__dirname, 'app/public/home.html'));
+	});
+
+	app.get('/survey', function (req, res) {
+	  res.sendFile(path.join(__dirname, 'app/public/survey.html'));
+	});
+}
